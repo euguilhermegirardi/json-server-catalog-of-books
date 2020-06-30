0464d230a967d61803dff4afca817e0ea68d1816
Guilherme Girardi <48716406+euguilhermegirardi@users.noreply.github.com>
2020-06-30 11:51:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const cors = require('cors');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3333;
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
